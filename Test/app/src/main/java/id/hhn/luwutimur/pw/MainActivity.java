5d775908f08a1d58d88a4f3784c7223eb4fb7cec
proker37 <nomore3790@gmail.com>
2016-04-13 18:15:49 +0700

    Test

---
+++ Test/app/src/main/java/id/hhn/luwutimur/pw/MainActivity.java
@@ -0,0 +1,14 @@
+package id.hhn.luwutimur.pw;
+
+import android.app.*;
+import android.os.*;
+
+public class MainActivity extends Activity 
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
